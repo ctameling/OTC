03166d927fc90ef1c75a3136c6e004ea5b852685
julia.naas <julia.naas@stud.uni-goettingen.de>
2020-12-03 13:53:05 +0100

    object-based: Code, Data, Figure for hand-sampled yeast data

--- code/script_figure_6_yeast.R
+++ code/script_figure_6_yeast.R
@@ -36,14 +36,15 @@ for (i in data_sets){
   picsB <- files[grepl("_640", files)]
   squassh_data <- files[grepl("Squassh_", files)]
   debias_data <- files[grepl("_GILI", files)]
+  object_coloc <- files[grepl("Object_coloc_", files)]
   
-  # # compute tplans
+  # compute tplans
   tplans <- OTC::calculate_tplans(data_path = data_path_i, picsA = picsA, picsB = picsB, output_path = output_path, output_name = i)
-  
+
   #------------------------ Pixel based colocalization data ----------------------------------------#
   n <- length(picsA)
-  
-  # Initialize coefficient arrays 
+
+  # Initialize coefficient arrays
   ICCS_M1 <- rep(NA,n)
   ICCS_M2 <- rep(NA,n)
   LI_DeBias <- rep(NA,n)
@@ -53,22 +54,22 @@ for (i in data_sets){
   Pearson_s_with_tresh <- rep(NA,n)
   Thresholded_Overlap_Coeff_ch1 <- rep(NA,n)
   Thresholded_Overlap_Coeff_ch2 <- rep(NA,n)
-  
+
   # Include precomputed DeBias LI Coefficient
   debias_coloc <- read.table(paste0(data_path_i,"/",debias_data), header = TRUE, sep = ",")
   colnames(debias_coloc) <- c("GI","LI")
   LI_DeBias <- debias_coloc$LI
-  
+
   # Include precomputed Squassh Colocalization Coefficient
   squassh_coloc <- read.table(paste0(data_path_i,"/",squassh_data), header = TRUE, sep =",")
   Thresholded_Overlap_Coeff_ch1 <- squassh_coloc$ColocObjectsNumber[squassh_coloc$Channel != 1]
   Thresholded_Overlap_Coeff_ch2 <- squassh_coloc$ColocObjectsNumber[squassh_coloc$Channel == 1]
-  
+
   for (j in 1:n) {
     # Read simulated Tiff Pictures
     picA <- readTIFF(paste0(data_path_i,"/",picsA[j]))
     picB <- readTIFF(paste0(data_path_i,"/",picsB[j]))
-    
+
     # Compute Colocalization Coefficients
     Pearson_s_Corr[j] <- Pcor(picA, picB)
     Pearson_s_with_tresh[j] <- PcorP(picA, picB)$PcorP
@@ -77,52 +78,85 @@ for (i in data_sets){
     ICCS_M1[j] <- M1_ICCS(picA, picB)
     ICCS_M2[j] <- M2_ICCS(picA, picB)
   }
-  
+
   coloc_data <- data.frame("Picture" = i,
                            "ICCS M1" = ICCS_M1,
-                           "ICCS M2" = ICCS_M2, 
+                           "ICCS M2" = ICCS_M2,
                            "LI (DeBias)" = LI_DeBias,
-                           "Mander's M1" = Mander_s_M1, 
+                           "Mander's M1" = Mander_s_M1,
                            "Mander's M2" = Mander_s_M2,
-                           "Pearson's Corr" = Pearson_s_Corr, 
-                           "Pearson's with tresh." = Pearson_s_with_tresh, 
-                           "Thresholded Overlap Coeff ch1" = Thresholded_Overlap_Coeff_ch1, 
+                           "Pearson's Corr" = Pearson_s_Corr,
+                           "Pearson's with tresh." = Pearson_s_with_tresh,
+                           "Thresholded Overlap Coeff ch1" = Thresholded_Overlap_Coeff_ch1,
                            "Thresholded Overlap Coeff ch2" = Thresholded_Overlap_Coeff_ch2)
   assign(paste0("coloc_data_",i), coloc_data)
-  
+
   coefficients <- c("ICCS M1", "ICCS M2", "LI (DeBias)",
                     "Mander's M1", "Mander's M2",
                     "Pearson's Corr.", "Pearson's with tresh.",
                     "Thresholded Overlap Coeff ch1", "Thresholded Overlap Coeff ch2")
-  
+
   meanvalue <- rep(NA,length(coefficients))
   standarderror <- rep(NA,length(coefficients))
-  
+
   # Compute mean Computed Colocalization level
-  meanvalue[1] <- mean(ICCS_M1) 
-  standarderror[1] <- stand.error(ICCS_M1) 
-  meanvalue[2] <- mean(ICCS_M2)  
-  standarderror[2] <- stand.error(ICCS_M2) 
-  meanvalue[3] <- mean(LI_DeBias[complete.cases(LI_DeBias)]) 
-  standarderror[3] <- stand.error(LI_DeBias[complete.cases(LI_DeBias)]) 
-  meanvalue[4] <- mean(Mander_s_M1) 
-  standarderror[4] <- stand.error(Mander_s_M1) 
-  meanvalue[5] <- mean(Mander_s_M2)     
-  standarderror[5] <- stand.error(Mander_s_M2) 
-  meanvalue[6] <- mean(Pearson_s_Corr) 
-  standarderror[6] <- stand.error(Pearson_s_Corr) 
-  meanvalue[7] <- mean(Pearson_s_with_tresh) 
-  standarderror[7] <- stand.error(Pearson_s_with_tresh) 
-  meanvalue[8] <- mean(Thresholded_Overlap_Coeff_ch1) 
-  standarderror[8] <- stand.error(Thresholded_Overlap_Coeff_ch1) 
-  meanvalue[9] <- mean(Thresholded_Overlap_Coeff_ch2) 
-  standarderror[9] <- stand.error(Thresholded_Overlap_Coeff_ch2) 
-  
-  name_molecule <- i 
-  
-  frame_coefficients <- data.frame(coefficients, meanvalue, standarderror, 
+  meanvalue[1] <- mean(ICCS_M1)
+  standarderror[1] <- stand.error(ICCS_M1)
+  meanvalue[2] <- mean(ICCS_M2)
+  standarderror[2] <- stand.error(ICCS_M2)
+  meanvalue[3] <- mean(LI_DeBias[complete.cases(LI_DeBias)])
+  standarderror[3] <- stand.error(LI_DeBias[complete.cases(LI_DeBias)])
+  meanvalue[4] <- mean(Mander_s_M1)
+  standarderror[4] <- stand.error(Mander_s_M1)
+  meanvalue[5] <- mean(Mander_s_M2)
+  standarderror[5] <- stand.error(Mander_s_M2)
+  meanvalue[6] <- mean(Pearson_s_Corr)
+  standarderror[6] <- stand.error(Pearson_s_Corr)
+  meanvalue[7] <- mean(Pearson_s_with_tresh)
+  standarderror[7] <- stand.error(Pearson_s_with_tresh)
+  meanvalue[8] <- mean(Thresholded_Overlap_Coeff_ch1)
+  standarderror[8] <- stand.error(Thresholded_Overlap_Coeff_ch1)
+  meanvalue[9] <- mean(Thresholded_Overlap_Coeff_ch2)
+  standarderror[9] <- stand.error(Thresholded_Overlap_Coeff_ch2)
+
+  name_molecule <- i
+
+  frame_coefficients <- data.frame(coefficients, meanvalue, standarderror,
                                    name_molecule, i, stringsAsFactors=FALSE)
   assign(paste0("frame_",i), frame_coefficients)
+
+  #------------------------ Object based colocalization data ----------------------------------------#
+  data_obj_coloc <- read.csv(paste0(data_path_i,"/",object_coloc), header = TRUE, sep = ";", dec=",")
+  Mask_center_1_inside_Mask_2 <- data_obj_coloc$X..of.1..center.of.mass..inside.masks.2
+  Ripley_s_K_of_2_coloc_with_1 <- data_obj_coloc$X...fit.of.K.function..of.2.coloc..with.1
+  SODA_of_2_coloc._with_1 <- data_obj_coloc$X...SODA..of.2.coloc..with.1
+
+  coloc_data <- data.frame("Picture" = i,
+                           "Mask center 1 inside Mask 2" = Mask_center_1_inside_Mask_2,
+                           "Ripley's K of 2 coloc. with 1" = Ripley_s_K_of_2_coloc_with_1,
+                           "SODA of 2 coloc. with 1" = SODA_of_2_coloc._with_1)
+  assign(paste0("coloc_data_object_",i), coloc_data)
+
+  coefficients <- c("Mask center 1 inside Mask 2",
+                    "Ripley's K function of 2 coloc. with 1",
+                    "SODA of 2 coloc. with 1")
+
+  meanvalue <- rep(NA,length(coefficients))
+  stand.error <- function(x) sd(x)/sqrt(length(x))
+  standarderror <- rep(NA,length(coefficients))
+
+  # Compute mean Computed Colocalization level
+  meanvalue[1] <- mean(Mask_center_1_inside_Mask_2)
+  standarderror[1] <- stand.error(Mask_center_1_inside_Mask_2)
+  meanvalue[2] <- mean(Ripley_s_K_of_2_coloc_with_1)
+  standarderror[2] <- stand.error(Ripley_s_K_of_2_coloc_with_1)
+  meanvalue[3] <- mean(SODA_of_2_coloc._with_1)
+  standarderror[3] <- stand.error(SODA_of_2_coloc._with_1)
+
+  name_molecule <- i
+  frame_coefficients <- data.frame(coefficients, meanvalue, standarderror,
+                                   name_molecule, i, stringsAsFactors=FALSE)
+  assign(paste0("frame_object_",i), frame_coefficients)
 }
 
 # evaluate OTC
@@ -144,10 +178,10 @@ coloc_complete <- rbind(coloc_data_Tom40_Mrpl4, coloc_data_Tom40_Cbp3,
 mean_complete$i <- factor(mean_complete$i, levels = unique(mean_complete$i), ordered=TRUE)
 
 # Initialize Barplot
-barplot <- ggplot(mean_complete, x=i, y=meanvalue, aes(i, meanvalue, fill = coefficients)) +  
-  geom_bar(stat = "identity", , position=position_dodge()) + 
+barplot <- ggplot(mean_complete, x=i, y=meanvalue, aes(i, meanvalue, fill = coefficients)) +
+  geom_bar(stat = "identity", , position=position_dodge()) +
   labs(title = "", x = "", y = "") +
-  geom_errorbar(aes(ymin=meanvalue-standarderror, ymax=meanvalue+standarderror), 
+  geom_errorbar(aes(ymin=meanvalue-standarderror, ymax=meanvalue+standarderror),
                 position = position_dodge()) +
   coord_cartesian(ylim = c(0, 2.5))
 
@@ -155,9 +189,34 @@ pdf("../results/yeast_figure6_pixelbased_comparison.pdf", width = 10, height = 7
 barplot
 dev.off()
 
-# Save Colocalization data 
-write.csv(coloc_complete, "../results/yeast_figure6_pixelbased_comparison_coloc_data.csv") 
+# Save Colocalization data
+write.csv(coloc_complete, "../results/yeast_figure6_pixelbased_comparison_coloc_data.csv")
 write.csv(mean_complete, "../results/yeast_figure6_pixelbased_comparison_mean_data.csv")
+
+#------------------------ Object based colocalization data ----------------------------------------#
+# Combine data of all Colocalization levels
+mean_object_complete <- rbind(frame_object_Tom40_Mrpl4, frame_object_Tom40_Cbp3,
+                       frame_object_Tom40_Tom20, frame_object_Tom40_Tom40)
+coloc_object_complete <- rbind(coloc_data_object_Tom40_Mrpl4, coloc_data_object_Tom40_Cbp3,
+                        coloc_data_object_Tom40_Tom20, coloc_data_object_Tom40_Tom40)
+mean_object_complete$i <- factor(mean_object_complete$i, 
+                                 levels = unique(mean_object_complete$i), ordered=TRUE)
+
+# Initialize Barplot
+barplot <- ggplot(mean_object_complete, x=factor(i), y=meanvalue, aes(factor(i), meanvalue, fill = coefficients)) +
+  geom_bar(stat = "identity", position=position_dodge()) +
+  labs(title = "", x = "", y = "") +
+  geom_errorbar(aes(ymin=meanvalue-standarderror, ymax=meanvalue+standarderror),
+                position = position_dodge()) +
+  coord_cartesian(ylim = c(0, 1))
+
+pdf("../results/yeast_figure6_objectbased_comparison.pdf", width = 10, height = 7)
+barplot
+dev.off()
+
+# Save Colocalization data
+write.csv(coloc_object_complete, "../results/yeast_figure6_objectbased_comparison_coloc_data.csv")
+write.csv(mean_object_complete, "../results/yeast_figure6_objectbased_comparison_mean_data.csv")
 
 ############ randomly picked data #########################################################################
 seed_Tom40_Cbp3 <- 21
